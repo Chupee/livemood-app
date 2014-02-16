50c284f1c78d92757f566a2d25bcb09249a23feb
Chupee <julie.pronzac@gmail.com>
2014-02-16 12:34:27 +0100

    Add ActionBarDrawerToggle behaviour.

--- src/com/example/livemood/AgendaActivity.java
+++ src/com/example/livemood/AgendaActivity.java
@@ -2,16 +2,24 @@ package com.example.livemood;
 
 import java.util.ArrayList;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.livemood.adapters.ConcertsListAdapter;
+import com.example.livemood.fragments.OpertingSystemFragment;
 import com.example.livemood.models.Artist;
 import com.example.livemood.models.Concert;
 import com.example.livemood.models.Dig;
@@ -23,8 +31,11 @@ import com.example.livemood.models.ReferenceArtist;
 public class AgendaActivity extends Activity {
 	
 	/* Drawer Navigation */
+	private String[] drawerItems;
 	private DrawerLayout drawerLayout;
 	private ListView drawerList;
+	private ActionBarDrawerToggle drawerToggle;
+	private CharSequence title;
 	
 	/* Concerts list */
 	private ArrayList<Concert> concertsList;
@@ -35,7 +46,14 @@ public class AgendaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_agenda);
-
+		
+		//
+		// Action bar
+		//
+	    ActionBar actionBar = getActionBar();
+	    actionBar.setHomeButtonEnabled(true);
+	    title = getActionBar().getTitle();
+	    
 		//
 		// Hard data (me gusta)
 		//
@@ -65,11 +83,34 @@ public class AgendaActivity extends Activity {
 		//
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawerList = (ListView) findViewById(R.id.left_drawer);
-		String[] drawerItems = getResources().getStringArray(R.array.items);
+		drawerItems = getResources().getStringArray(R.array.items);
 		// Set the adapter for the list view
         drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, R.id.itemName, drawerItems));
         // Set the list's click listener
-        //drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        // Drawer toggle
+        drawerToggle = new ActionBarDrawerToggle(this,               
+                drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */) {
+
+        	/** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                // getActionBar().setTitle(title);
+            }
+            
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                // getActionBar().setTitle("Open Drawer");
+            }
+        };
+        
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(drawerToggle);
+        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
         
 		//
 		// List
@@ -90,7 +131,66 @@ public class AgendaActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.agenda, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
+	
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+        switch (item.getItemId()) {
+		    case R.id.action_settings:
+		      Toast.makeText(this, "Settings selected", Toast.LENGTH_LONG).show();
+		      break;
+		
+		    default:
+		      break;
+		}
+        return super.onOptionsItemSelected(item);
+    }
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+	
+	/** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // create a new fragment and specify the planet to show based on position
+        Fragment fragment = new OpertingSystemFragment();
+        Bundle args = new Bundle();
+        args.putInt(OpertingSystemFragment.ARG_OS, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                       .replace(R.id.content_frame, fragment)
+                       .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        drawerList.setItemChecked(position, true);
+        getActionBar().setTitle((drawerItems[position]));
+        drawerLayout.closeDrawer(drawerList);
+    }
 
 }
