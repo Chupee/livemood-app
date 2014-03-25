2602ba2d6d54b60019644649ccafa6593d35ee8c
Pierre Criulanscy <pcriulan@gmail.com>
2014-03-25 18:04:44 +0100

    concert info on ConcertDetailsFragment are now retrieved from Parse !  (need to handle tags now)

--- src/com/example/livemood/fragments/ConcertDetailsFragment.java
+++ src/com/example/livemood/fragments/ConcertDetailsFragment.java
@@ -1,7 +1,5 @@
 package com.example.livemood.fragments;
 
-import java.util.ArrayList;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,26 +10,38 @@ import android.widget.TextView;
 import com.example.livemood.R;
 import com.example.livemood.models.Artist;
 import com.example.livemood.models.Concert;
-import com.example.livemood.models.Dig;
 import com.example.livemood.models.Label;
-import com.example.livemood.models.Mood;
 import com.example.livemood.models.Place;
-import com.example.livemood.models.ReferenceArtist;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class ConcertDetailsFragment extends Fragment {
 	
 	private final String TITLE = "Concert";
-	private int concertId;
+	private String concertId;
+	private Concert concert;
 	
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
     Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.concert_fragment, null);
+    final View view = inflater.inflate(R.layout.concert_fragment, null);
     
     // Concert Id
-    concertId = getArguments().getInt("concertId", 0);
+    concertId = getArguments().getString("concertId");
+    
+    /*
+     * BESOIN DE :
+     * label
+     * artist
+     * tags
+     * place
+     *  
+     *
+     */
 	try {
-		if(concertId == 0) { throw new Exception("Erreur : le concert s�lectionn� n'existe pas."); }
+		if(concertId == "") { throw new Exception("Erreur : le concert s�lectionn� n'existe pas."); }
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
@@ -39,70 +49,47 @@ public class ConcertDetailsFragment extends Fragment {
     // Update action bar
     getActivity().getActionBar().setTitle(TITLE);
     
-    //
-	// Hard data (me gusta)
-	/*Label labello = new Label("Labello", "labello.png", new ArrayList<Artist>());
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("concert");
+	query.include("place");
+	query.include("artist");
+	query.include("artist.label");
 	
-	Mood sexy = new Mood("Sexy", new ArrayList<Artist>());
-	Mood chevaleresque = new Mood("chevaleresque", new ArrayList<Artist>());
-	Mood rebelle = new Mood("Rebelle", new ArrayList<Artist>());
-	
-	Artist birdyHunt = new Artist("Birdy Hunt", "birdyhunt.png", "birdyhunt-cover.png", labello, new ArrayList<Dig>(), new ArrayList<Concert>(), new ArrayList<Mood>(), new ArrayList<ReferenceArtist>());
-	birdyHunt.getMoodsList().add(sexy);
-	birdyHunt.getMoodsList().add(rebelle);
-	birdyHunt.getMoodsList().add(chevaleresque);
-	rebelle.getArtistsList().add(birdyHunt);
-	chevaleresque.getArtistsList().add(birdyHunt);
-	sexy.getArtistsList().add(birdyHunt);
-	labello.getArtistsList().add(birdyHunt);
-	
-	Artist fifou = new Artist("Fifou Odrezal", "birdyhunt.png", "birdyhunt-cover.png", labello, new ArrayList<Dig>(), new ArrayList<Concert>(), new ArrayList<Mood>(), new ArrayList<ReferenceArtist>());
-	fifou.getMoodsList().add(sexy);
-	fifou.getMoodsList().add(rebelle);
-	fifou.getMoodsList().add(chevaleresque);
-	rebelle.getArtistsList().add(fifou);
-	chevaleresque.getArtistsList().add(fifou);
-	sexy.getArtistsList().add(fifou);
-	labello.getArtistsList().add(fifou);
-	
-	Place bataclan = new Place("Bataclan", "28 rue du Swag", new ArrayList<Concert>());
-	
-	Concert concert1 = new Concert(123456789, birdyHunt, bataclan, "2013-11-28", "concert1.png");
-	Concert concert2 = new Concert(223456789, fifou, bataclan, "2013-11-28", "concert1.png");
-	Concert concert3 = new Concert(323456789, birdyHunt, bataclan, "2013-11-28", "concert1.png");
-	Concert concert4 = new Concert(423456789, fifou, bataclan, "2013-11-28", "concert1.png");
-	birdyHunt.getConcertsList().add(concert1);
-	birdyHunt.getConcertsList().add(concert3);
-	fifou.getConcertsList().add(concert2);
-	fifou.getConcertsList().add(concert4);
-	
-	bataclan.getConcertsList().add(concert1);
-	bataclan.getConcertsList().add(concert2);
-	bataclan.getConcertsList().add(concert3);
-	bataclan.getConcertsList().add(concert4);*/
-	
-	//
-	// List
-	//
-	ArrayList<Concert> concertsList = new ArrayList<Concert>();
-	/*concertsList.add(concert1);
-	concertsList.add(concert2);
-	concertsList.add(concert3);
-	concertsList.add(concert4);*/
-    
-    // Concert
-    Concert concert = concertsList.get(1); // TODO : get the concert in db depending on the concertId
-    
-    //TextView
-    TextView tvArtistName = (TextView) view.findViewById(R.id.artistName);
-    TextView tvArtistLabel = (TextView) view.findViewById(R.id.artistLabel);
-    TextView tvDateAndPlace = (TextView) view.findViewById(R.id.dateAndPlace);
-    tvArtistName.setText(concert.getArtist().getName());
-    tvArtistLabel.setText(concert.getArtist().getLabel().getName());
-    tvDateAndPlace.setText(concert.getDate()+" - "+concert.getPlace().getName());
+	query.getInBackground(concertId, new GetCallback<ParseObject>() {
+		
+		@Override
+		public void done(ParseObject arg0, ParseException arg1) {
+			// TODO Auto-generated method stub
+			ParseObject parseConcert = new ParseObject("concert");
+			ParseObject parseArtist = new ParseObject("artist");
+			ParseObject parseLabel = new ParseObject("label");
+			ParseObject parsePlace = new ParseObject("place");
+			
+			Artist artist = null;
+			Place place = null;
+			Label label = null;
+			
+			parseConcert = arg0;
+			parseArtist = parseConcert.getParseObject("artist");
+			parseLabel = parseArtist.getParseObject("label");
+			parsePlace = parseConcert.getParseObject("place");
+			label = new Label(parseLabel.get("name").toString(), "");
+			artist = new Artist(parseArtist.get("name").toString(), "", "", label);
+			place = new Place(parsePlace.get("name").toString(), "");
+			concert = new Concert(parseConcert.getObjectId(), artist, place, parseConcert.get("date").toString(), "");
+			
+			//TextView
+		    TextView tvArtistName = (TextView) view.findViewById(R.id.artistName);
+		    TextView tvArtistLabel = (TextView) view.findViewById(R.id.artistLabel);
+		    TextView tvDateAndPlace = (TextView) view.findViewById(R.id.dateAndPlace);
+		    tvArtistName.setText(concert.getArtist().getName());
+		    tvArtistLabel.setText(concert.getArtist().getLabel().getName());
+		    tvDateAndPlace.setText(concert.getDate()+" - "+concert.getPlace().getName());
+		}
+		
+	});
 
-    
     return view;
+    
   }
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
